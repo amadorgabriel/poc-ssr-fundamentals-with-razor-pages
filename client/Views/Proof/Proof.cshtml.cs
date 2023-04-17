c20dd0069bd7d77f5988c67ca1b45a20773c8e8e
Gabriel Amador <gabrielramador2014@gmail.com>
2023-04-16 22:25:42 -0300

    feat: add proof page

---
+++ client/Views/Proof/Proof.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace client.Views.Home
+{
+    public class ProofModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
