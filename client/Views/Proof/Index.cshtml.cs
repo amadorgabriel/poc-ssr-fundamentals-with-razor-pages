ec03232a2881819352cde26f34a124b7bd47d87a
Gabriel Amador <gabrielramador2014@gmail.com>
2023-04-19 14:33:19 -0300

    style: add proof page styles

--- client/Views/Proof/Index.cshtml.cs
+++ client/Views/Proof/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace client.Views.Home
+namespace client.Views.Proof
 {
     public class ProofModel : PageModel
     {
