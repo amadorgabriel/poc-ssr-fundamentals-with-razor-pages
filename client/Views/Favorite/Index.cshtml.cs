027cbe0a102a8db0a7a59840dec19bd7654ab2b7
Gabriel Amador <gabrielramador2014@gmail.com>
2023-04-20 16:55:40 -0300

    feat: add favorite page

---
+++ client/Views/Favorite/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace client.Views.Favorite
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+     
+        }
+    }
+}
