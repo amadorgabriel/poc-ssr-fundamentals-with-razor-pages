af040558eab7bd6d31ac4977b7cccf2105376e73
Gabriel Amador <gabrielramador2014@gmail.com>
2023-04-20 16:56:27 -0300

    feat: small changes

---
+++ client/ViewComponents/Callout/CalloutViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using client.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace client.ViewComponents.Callout
+{
+    public class CalloutViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string message)
+        {
+            CalloutViewModel viewModel = new CalloutViewModel{Message = message};
+         
+            if(message == null)
+            {
+                viewModel.Message = "Teste msg default";  
+            }
+
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ client/ViewComponents/Callout/IconViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using client.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace client.ViewComponents.Callout
+{
+    public class IconViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string message)
+        {
+			IconViewModel viewModel = new IconViewModel{2};
+         
+            if(message == null)
+            {
+                viewModel = 2;
+            }
+
+
+            return View(viewModel);
+        }
+    }
+}
